34b933a8c963359a3dd2c3e7375bfa6406626e9e
André Blos Aliatti <andreblos82@gmail.com>
2026-02-11 10:13:11 -0300

    Catch request error

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,7 +2,7 @@ import "./styles.css"
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import type { ProductDTO } from "../../../models/product";
 import * as productService from "./../../../services/product-service"
@@ -11,6 +11,8 @@ export default function ProductDetails(){
 
   const params = useParams();
 
+  const navigate = useNavigate();
+
   const [product, setProduct] = useState<ProductDTO>();
 
   useEffect(() => {
@@ -18,7 +20,11 @@ export default function ProductDetails(){
       .then(response => {
         console.log(response.data)
         setProduct(response.data)
-      });}, []);
+      })
+      .catch (() => {
+        navigate("/")
+      });
+    }, []);
 
    
     return( 
